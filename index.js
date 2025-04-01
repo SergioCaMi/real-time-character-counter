013c0ea995829bbec2d12b6616b66dc1cc8aa550
SergioCaMi <sergiocami84@gmail.com>
2025-04-01 22:02:18 +0200

    fin ejercicio

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Buena suerte ! :)
+const characterMaxLength = document.getElementById("textarea").getAttribute("maxlength");
+
+
+const txtArea = document.getElementById("textarea");
+
+txtArea.addEventListener("input", e=>{
+    document.getElementById("total-counter").textContent = (document.getElementById("textarea").value).length;
+
+    document.getElementById("remaining-counter").textContent = (document.getElementById("textarea").value).length - characterMaxLength;
+
+});
+
+
+
